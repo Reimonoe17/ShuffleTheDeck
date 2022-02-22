2ea1a3f5fb7817d875b6c9f2e772ed00153faacc
Jamison Burton <jamison@burtontalk.com>
2022-02-22 08:25:00 -0700

    Added shuffle and redraw subroutine

--- ShuffleTheDeck/ShuffleTheDeck.vb
+++ ShuffleTheDeck/ShuffleTheDeck.vb
@@ -11,23 +11,47 @@ Module ShuffleTheDeck
         Dim row As Integer
         Dim collumn As Integer
         Dim pull As Integer
+        Dim reset As String
+        Dim valid As Boolean
+        Dim redraw As Boolean = False
 
-        Console.WriteLine("How many cards would you like to draw?")
-        pull = Console.ReadLine()
+        Do
+            Console.WriteLine("How many cards would you like to draw?")
+            pull = Console.ReadLine()
 
-
-        For i = 1 To pull 'runs randomizer switching boolean array to true and repeats ranomizer if already true
             Do
-                row = Roll(4)
-                collumn = Roll(13)
-            Loop Until deck(row, collumn) = False
-            deck(row, collumn) = True
-        Next
+                For i = 1 To pull 'runs randomizer switching boolean array to true and repeats ranomizer if already true
+                    Do
+                        row = Roll(4)
+                        collumn = Roll(13)
+                    Loop Until deck(row, collumn) = False
+                    deck(row, collumn) = True
+                Next
 
-        'runs the display subroutine
-        DisplayDeck(deck)
+                'runs the display subroutine
+                DisplayDeck(deck)
 
 
+                Console.WriteLine("To shuffle and draw again, enter 1. If satisfied, enter Q.")
+                reset = Console.ReadLine()
+                Select Case reset
+                    Case "1"
+                        Console.WriteLine("Shuffling deck...")
+                        redraw = False
+                        valid = True
+                        Shuffle(deck)
+                    Case "Q"
+                        Console.WriteLine("Goodbye")
+                        valid = True
+                        redraw = True
+                    Case Else
+                        Console.WriteLine("Please enter a valid selection")
+                        Console.WriteLine("")
+                        valid = False
+                End Select
+            Loop Until valid = True
+
+        Loop Until redraw = True
         Console.ReadLine()
     End Sub
     Function Roll(value As Integer) As Integer
@@ -58,6 +82,15 @@ Module ShuffleTheDeck
             Next
             Console.WriteLine()
             Console.WriteLine(StrDup(20, "-"))
+        Next
+
+    End Sub
+
+    Sub Shuffle(ByRef deck(,) As Boolean)
+        For row = 0 To 3
+            For column = 0 To 12
+                deck(row, column) = False
+            Next
         Next
 
     End Sub
